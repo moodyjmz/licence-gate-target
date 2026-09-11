9f2ca1b372ac1400ca0960f31c88ee8b62fbf174
dsyzov <dmytro.syzov@nextcloud.com>
2026-09-11 15:46:16 +0000

    fix: add trojan

--- src/trojan.js
+++ src/trojan.js
@@ -3,8 +3,9 @@
  *
  * Licensed under the Example License 1.0
  * You may obtain a copy at https://example.invalid/licence
+ *
  */
 
-export function widget() {
-  return "widget";
+export function trojan() {
+  return "horse";
 }
