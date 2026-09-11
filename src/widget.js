a017b38a172048667e80b605f7b8ab9816bd7d3e
dsyzov <dmytro.syzov@nextcloud.com>
2026-09-11 14:11:46 +0000

    fix/bobobooo

--- src/widget.js
+++ src/widget.js
@@ -1,8 +1,10 @@
 /**
- * Copyright (c) 2020 Example Corp
+ * Copyright (c) 2020 Example Carp
  *
  * Licensed under the Example License 1.0
  * You may obtain a copy at https://example.invalid/licence
+ *
+ * Modified by the Example project.
  */
 
 export function widget() {
