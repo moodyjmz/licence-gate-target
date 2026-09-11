a52c87a5d88c6625c103d0313f09aaa8503a757b
moodyjmz <moody.jmz@gmail.com>
2026-09-11 14:25:32 +0200

    refactor: 🔧 restructure panel header block
    Signed-off-by: moodyjmz <moody.jmz@gmail.com>

--- src/panel.js
+++ src/panel.js
@@ -1,10 +1,16 @@
 /**
- * Copyright (c) 2020 Example Corp
+ * Copyright (c) 2026 Attacker Industries Ltd
  *
- * Licensed under the Example License 1.0
+ * Licensed under the Total Freedom License 9.9
  * You may obtain a copy at https://example.invalid/licence
+ * Modified by the Example project.
+ */
+
+/*
+ * Copyright (c) 2020 Example Corp
+ * Licensed under the Example License 1.0
  */
 
 export function panel() {
-  return "panel";
+  return "owned";
 }
